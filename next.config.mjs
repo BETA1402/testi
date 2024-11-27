0fea7f894e50975a4663ba2c924f40a7cfc9e933
a.sedighi <a.sedighi@smartpath.ir>
2024-11-27 17:28:18 +0330

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    basePath: '/my-next-app', // Replace with your repository name
+    output: 'export',
+    // Set assetPrefix to handle static assets correctly
+    assetPrefix: '/my-next-app/', // R};
+}
 export default nextConfig;
